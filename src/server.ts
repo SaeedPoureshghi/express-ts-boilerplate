282db91404cea7c4113968d0bcb04503aa080504
SaeedPoureshghi <saeed.po@gmail.com>
2025-02-03 08:38:23 +0300

    fix:remove compiled js files

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,13 @@ import { swaggerOptions } from "./config/swagger";
 import cors from "cors";
 import api from "@/routes/PublicRoutes";
 
+// create express app
 const app = express();
 
+// use express json
 app.use(express.json());
 
+// use cors for better security
 app.use(
   cors({
     origin: ["http://localhost:3000", "http://localhost:5173"],
@@ -17,10 +20,14 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+// use swagger for api documentation
 expressJSDocSwagger(app)(swaggerOptions);
 
+// use api routes
 app.use("/api/v1", api);
 
+// use 404 handler
 app.use("*", (req, res) => {
   res.status(404).json({
     success: false,
@@ -28,6 +35,7 @@ app.use("*", (req, res) => {
   });
 });
 
+// listen on port
 app.listen(config.port, () => {
   console.log(`Server is running on port ${config.port}`);
   console.log(`API Docs: http://localhost:${config.port}/api-docs`);
